d40e605e988f9be5cbf3d04ec008a7d316f95bf9
Jia Hao <jiahaog@gmail.com>
2015-07-05 14:17:40 +0800

    Reformattted code with tabs and colons

--- cli.js
+++ cli.js
@@ -1,32 +1,32 @@
 #!/usr/bin/env node
-var fs = require('fs')
-var args = require('minimist')(process.argv.slice(2), {boolean: ['prune', 'asar', 'all', 'overwrite']})
-var packager = require('./')
-var usage = fs.readFileSync(__dirname + '/usage.txt').toString()
+var fs = require('fs');
+var args = require('minimist')(process.argv.slice(2), {boolean: ['prune', 'asar', 'all', 'overwrite']});
+var packager = require('./');
+var usage = fs.readFileSync(__dirname + '/usage.txt').toString();
 var validator = require('validator');
 
 args.dir = './app';
 args.name = args._[0];
 
-var protocolSchemes = [].concat(args.protocol || [])
-var protocolNames = [].concat(args['protocol-name'] || [])
+var protocolSchemes = [].concat(args.protocol || []);
+var protocolNames = [].concat(args['protocol-name'] || []);
 
 if (protocolSchemes && protocolNames && protocolNames.length === protocolSchemes.length) {
-  args.protocols = protocolSchemes.map(function (scheme, i) {
-    return {schemes: [scheme], name: protocolNames[i]}
-  })
+    args.protocols = protocolSchemes.map(function (scheme, i) {
+        return {schemes: [scheme], name: protocolNames[i]};
+    })
 }
 
 
 if (!validator.isURL(args.target)) {
     console.error('Enter a valid target url');
-    process.exit(1)
+    process.exit(1);
 }
 
 if (!args.dir || !args.name || !args.version || !args.target || (!args.all && (!args.platform || !args.arch))) {
     console.error(usage);
 
-      process.exit(1)
+    process.exit(1);
 }
 
 var appArgs = {
@@ -37,13 +37,13 @@ var appArgs = {
 fs.writeFileSync('./app/targetUrl.txt', JSON.stringify(appArgs));
 
 
-packager(args, function done (err, appPaths) {
-  if (err) {
-    if (err.message) console.error(err.message)
-    else console.error(err, err.stack)
-    process.exit(1)
-  }
+packager(args, function done(err, appPaths) {
+    if (err) {
+        if (err.message) console.error(err.message);
+        else console.error(err, err.stack);
+        process.exit(1);
+    }
 
-  if (appPaths.length > 1) console.error('Wrote new apps to:\n' + appPaths.join('\n'))
-  else if (appPaths.length === 1) console.error('Wrote new app to', appPaths[0])
-})
+    if (appPaths.length > 1) console.error('Wrote new apps to:\n' + appPaths.join('\n'));
+    else if (appPaths.length === 1) console.error('Wrote new app to', appPaths[0]);
+});
