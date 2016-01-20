a65bf351bc4269eeffcb509b6f1a1ed1f82524d6
Matt Prestlien <mattchuepicchu@gmail.com>
2016-01-20 08:54:05 -0800

    Allow intranet URLs
    This could be a great way to package intranet applications into electron apps. Added `require_tld` validator config setting to allow those intranet locations without a TLD to be packaged.

--- src/options.js
+++ src/options.js
@@ -115,7 +115,7 @@ function normalizeUrl(testUrl) {
     if (!parsed.protocol) {
         normalized = 'http://' + normalized;
     }
-    if (!validator.isURL(normalized, {require_protocol: true})) {
+    if (!validator.isURL(normalized, {require_protocol: true, require_tld: true})) {
         throw `Your Url: "${normalized}" is invalid!`;
     }
     return normalized;
