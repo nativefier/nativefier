2620e784278c77f9c4d5f8b49e07e45d2bcab92f
Jia Hao <jiahaog@gmail.com>
2016-01-22 10:10:56 +0800

    Don't commit node_modules

--- app/main.js
+++ app/main.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var os = require('os');
 var electron = require('electron');
 
-var wurl = require('./lib/wurl');
+var wurl = require('wurl');
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
 var shell = electron.shell;
