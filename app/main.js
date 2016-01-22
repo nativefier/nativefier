d7f04e8b65b73190dc4cb69d781cc7fea41b08cc
Jia Hao <jiahaog@gmail.com>
2016-01-22 19:05:42 +0800

    Fix #46 Url is not defined

--- app/main.js
+++ app/main.js
@@ -97,7 +97,7 @@ app.on('ready', function () {
             return;
         }
         event.preventDefault();
-        shell.openExternal(url);
+        shell.openExternal(urlToGo);
     });
 
     mainWindow.loadURL(appArgs.targetUrl);
