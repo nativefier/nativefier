a3f9a0411c4b3c59ede9f68bf0c5ae5101b9c193
Goh Jia Hao <jiahaog@gmail.com>
2016-05-14 17:28:43 +0800

    Remove deprecated electron api from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,4 @@
-var electronPublicApi = [
-    'electron',
-    // for modules which use deprecated api
-    // modules are taken from https://github.com/atom/electron/tree/master/docs/api
-    // 'app',
-    // 'screen'
-
-    // Uncomment as needed if some external module uses the deprecated api
-
-    // 'accelerator',
-    // 'auto-updater',
-    // 'browser-window',
-    // 'chrome-command-line-switches',
-    // 'clipboard',
-    // 'content-tracing',
-    // 'crash-reporter',
-    // 'desktop-capturer',
-    // 'dialog',
-    // 'download-item',
-    // 'environment-variables',
-    // 'file-object',
-    // 'frameless-window',
-    // 'global-shortcut',
-    // 'ipc-main',
-    // 'ipc-renderer',
-    // 'menu-item',
-    // 'menu',
-    // 'native-image',
-    // 'power-monitor',
-    // 'power-save-blocker',
-    // 'process',
-    // 'protocol',
-    // 'remote',
-    // 'session',
-    // 'shell',
-    // 'synopsis',
-    // 'tray',
-    // 'web-contents',
-    // 'web-frame',
-    // 'web-view-tag',
-    // 'window-open'
-];
+var electronPublicApi = ['electron'];
 
 var nodeModules = {};
 electronPublicApi.forEach(apiString => {
