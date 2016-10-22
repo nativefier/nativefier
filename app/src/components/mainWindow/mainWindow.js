bacd7bd1a95872d8ae07c1cd078c1c9099b42b91
texot <tete1030@gmail.com>
2016-10-22 20:50:23 +0800

    fix "Infix operators must be spaced"

--- app/src/components/mainWindow/mainWindow.js
+++ app/src/components/mainWindow/mainWindow.js
@@ -25,7 +25,7 @@ function createMainWindow(options, onAppQuit, setDockBadge) {
 
     const mainWindow = new BrowserWindow({
         frame: !options.hideWindowFrame,
-        titleBarStyle: options.hideTitleBar?'hidden':'default',
+        titleBarStyle: options.hideTitleBar ? 'hidden' : 'default',
         width: mainWindowState.width,
         height: mainWindowState.height,
         minWidth: options.minWidth,
