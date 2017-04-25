461c7a38f04ac578fc7291a77d1eb77a90ecb84f
Darren Haken <darrenhaken@gmail.com>
2017-04-25 16:04:57 +0100

    Change Mocha to not need a babel build to run (#349)
    * Change Mocha to not need a babel build to run
    
    - Also add tests around normalizeUrl
    
    * PR 359 Apply changes due to comments
    
    - Remove babelrc as its in the package.json
    - Change tdd npm task to use gulp
    - Remove source map support file from import list for normalizeUrlSpec
    - Change gulp tdd task to run mocha on first run
     359 Apply changes due to comments
    
     - Remove babelrc as its in the package.json
     - Change tdd npm task to use gulp
     - Remove source map support file from import list for normalizeUrlSpec
     - Change gulp tdd task to run mocha on first run
     359 Apply changes due to comments
    
     - Remove babelrc as its in the package.json
     - Change tdd npm task to use gulp
     - Remove source map support file from import list for normalizeUrlSpec
     - Change gulp tdd task to run mocha on first run

--- gulp/tests/mocha.js
+++ gulp/tests/mocha.js
@@ -2,21 +2,31 @@ import gulp from 'gulp';
 import PATHS from './../helpers/src-paths';
 
 import istanbul from 'gulp-istanbul';
+import {Instrumenter} from 'isparta';
 import mocha from 'gulp-mocha';
 
-gulp.task('mocha', ['build'], done => {
-    gulp.src(PATHS.CLI_DEST_JS)
-        .pipe(istanbul({includeUntested: true}))
+gulp.task('mocha', done => {
+    gulp.src([PATHS.CLI_SRC_JS, '!src/cli.js'])
+        .pipe(istanbul({
+            instrumenter: Instrumenter,
+            includeUntested: true
+        }))
+        .pipe(istanbul.hookRequire()) // Force `require` to return covered files
         .on('finish', () => {
-            return gulp.src(PATHS.TEST_DEST_JS, {read: false})
-                .pipe(mocha())
+            return gulp.src(PATHS.TEST_SRC, {read: false})
+                .pipe(mocha({
+                    compilers: 'js:babel-core/register',
+                    recursive: true
+                }))
                 .pipe(istanbul.writeReports({
                     dir: './coverage',
                     reporters: ['lcov'],
                     reportOpts: {dir: './coverage'}
                 }))
-                .on('finish', () => {
-                    done();
-                });
+                .on('end', done);
         });
+});
+
+gulp.task('tdd', ['mocha'], () => {
+    return gulp.watch(['src/**/*.js', 'test/**/*.js'], ['mocha']);
 });
--- src/options/normalizeUrl.js
+++ src/options/normalizeUrl.js
@@ -1,23 +1,26 @@
 import url from 'url';
 import validator from 'validator';
 
-function normalizeUrl(testUrl) {
-    // add protocol if protocol not found
-    let normalized = testUrl;
-    const parsed = url.parse(normalized);
+function appendProtocol(testUrl) {
+    const parsed = url.parse(testUrl);
     if (!parsed.protocol) {
-        normalized = 'http://' + normalized;
+        return `http://${testUrl}`;
     }
+    return testUrl;
+}
+
+function normalizeUrl(testUrl) {
+    const urlWithProtocol = appendProtocol(testUrl);
 
     const validatorOptions = {
         require_protocol: true,
         require_tld: false,
         allow_trailing_dot: true // mDNS addresses, https://github.com/jiahaog/nativefier/issues/308
     };
-    if (!validator.isURL(normalized, validatorOptions)) {
-        throw `Your Url: "${normalized}" is invalid!`;
+    if (!validator.isURL(urlWithProtocol, validatorOptions)) {
+        throw `Your Url: "${urlWithProtocol}" is invalid!`;
     }
-    return normalized;
+    return urlWithProtocol;
 }
 
 export default normalizeUrl;
