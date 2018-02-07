fc4d3659879a498f720e2d6e468ce405cccc9b1c
Ronan Jouchet <ronan@jouchet.fr>
2018-02-06 21:56:39 -0500

    Update deps, default to Electron 1.8.2 stable
    Semver-major deps upgrades: babel-jest electron-packager gulp-mocha jest shelljs

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 import path from 'path';
 
 export const DEFAULT_APP_NAME = 'APP';
-export const ELECTRON_VERSION = '1.7.9';
+export const ELECTRON_VERSION = '1.8.2';
 export const PLACEHOLDER_APP_DIR = path.join(__dirname, './../', 'app');
