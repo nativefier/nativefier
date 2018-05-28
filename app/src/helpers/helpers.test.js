9e587e5fe3887a431a103b3ca00ec8c741df75b7
David Kramer <bacongravy@icloud.com>
2018-05-28 05:14:54 -0700

    Fix #610 - Make the counter regexp allow punctuation (#626)
    When using an app such as Gmail, the unread count is included in the title, and the --counter feature displays that number on the dock icon. However, when the number is larger than 999, it includes commas in the number (e.g. "1,000"), and the number is no longer displayed on the dock icon, because the regular expression used to detect the counter value does not permit punctuation. This change modifies the regular expression used to match the counter value to permit "." and ",".

--- app/src/helpers/helpers.test.js
+++ app/src/helpers/helpers.test.js
@@ -1,6 +1,6 @@
 import helpers from './helpers';
 
-const { linkIsInternal } = helpers;
+const { linkIsInternal, getCounterValue } = helpers;
 
 const internalUrl = 'https://medium.com/';
 const internalUrlSubPath = 'topic/technology';
@@ -27,4 +27,20 @@ test('urls from different sites should not be internal', () => {
 
 test('all urls should be internal with wildcard regex', () => {
   expect(linkIsInternal(internalUrl, externalUrl, wildcardRegex)).toEqual(true);
+});
+
+const smallCounterTitle = 'Inbox (11) - nobody@example.com - Gmail';
+const largeCounterTitle = 'Inbox (8,756) - nobody@example.com - Gmail';
+const noCounterTitle = 'Inbox - nobody@example.com - Gmail';
+
+test('getCounterValue should return undefined for titles without counter numbers', () => {
+  expect(getCounterValue(noCounterTitle)).toEqual(undefined);
+});
+
+test('getCounterValue should return a string for small counter numbers in the title', () => {
+  expect(getCounterValue(smallCounterTitle)).toEqual('11');
+});
+
+test('getCounterValue should return a string for large counter numbers in the title', () => {
+  expect(getCounterValue(largeCounterTitle)).toEqual('8,756');
 });
