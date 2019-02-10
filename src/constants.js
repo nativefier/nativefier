726f44d2661c2664de5df236c69a64b6b3b64996
Ronan Jouchet <ronan@jouchet.fr>
2019-02-09 21:46:15 -0500

    Bump default Electron to 3.1.3

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 import path from 'path';
 
 export const DEFAULT_APP_NAME = 'APP';
-export const ELECTRON_VERSION = '3.0.13';
+export const ELECTRON_VERSION = '3.1.3';
 export const PLACEHOLDER_APP_DIR = path.join(__dirname, './../', 'app');
