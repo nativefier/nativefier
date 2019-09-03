f0399d9e9a36974c9d89252ebb1766daff3b1e5d
Filip Oščádal <filip@oscadal.com>
2019-09-03 02:43:48 +0200

    better example app

--- build_example_app.bat
+++ build_example_app.bat
@@ -1,2 +1,3 @@
 mkdir builds
-docker run -v %cd%\builds:/target local/nativefier -n "messenger" -a x64 --disable-context-menu --build-version 1.0 --electron-version 6.0.7 --disk-cache-size 52428800 --single-instance -p windows https://messenger.com/ /target/
+docker run -v %cd%\builds:/target -v %cd%\icons:/icons local/nativefier -n "messenger" -a x64 --disable-context-menu --build-version 1.0 --electron-version 6.0.7 --disk-cache-size 52428800 --single-instance -p linux -i /icons/messenger.png https://messenger.com /target/
+docker run -v %cd%\builds:/target -v %cd%\icons:/icons local/nativefier -n "messenger" -a x64 --disable-context-menu --build-version 1.0 --electron-version 6.0.7 --disk-cache-size 52428800 --single-instance -p windows -i /icons/messenger.ico https://messenger.com /target/
