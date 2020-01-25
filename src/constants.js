8bd32733f71298985a539bfa55e2ea809e134199
Ronan Jouchet <ronan@jouchet.fr>
2020-01-25 15:50:44 -0500

    remove constants probably back from a merge

--- src/constants.js
+++
@@ -1,5 +0,0 @@
-import path from 'path';
-
-export const DEFAULT_APP_NAME = 'APP';
-export const ELECTRON_VERSION = '5.0.13';
-export const PLACEHOLDER_APP_DIR = path.join(__dirname, './../', 'app');
