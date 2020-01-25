769a461a908eb31f89a9ea667846321362c9b200
Ronan Jouchet <ronan@jouchet.fr>
2020-01-25 14:40:29 -0500

    fields/icon.test

--- src/options/fields/icon.test.ts
+++ src/options/fields/icon.test.ts
@@ -7,37 +7,49 @@ jest.mock('./../../infer/inferIcon');
 jest.mock('loglevel');
 
 const mockedResult = 'icon path';
+const ICON_PARAMS_PROVIDED = {
+  icon: './icon.png',
+};
+const ICON_PARAMS_NEEDS_INFER = {
+  targetUrl: 'https://google.com',
+  platform: 'mac',
+};
 
 describe('when the icon parameter is passed', () => {
   test('it should return the icon parameter', async () => {
     expect(inferIcon).toHaveBeenCalledTimes(0);
-
-    const params = { icon: './icon.png' };
-    await expect(icon(params)).resolves.toBe(params.icon);
+    await expect(icon(ICON_PARAMS_PROVIDED)).resolves.toBe(
+      ICON_PARAMS_PROVIDED.icon,
+    );
   });
 });
 
 describe('when the icon parameter is not passed', () => {
   test('it should call inferIcon', async () => {
-    inferIcon.mockImplementationOnce(() => Promise.resolve(mockedResult));
-    const params = { targetUrl: 'some url', platform: 'mac' };
-
-    const result = await icon(params);
+    (inferIcon as jest.Mock).mockImplementationOnce(() =>
+      Promise.resolve(mockedResult),
+    );
+    const result = await icon(ICON_PARAMS_NEEDS_INFER);
 
     expect(result).toBe(mockedResult);
-    expect(inferIcon).toHaveBeenCalledWith(params.targetUrl, params.platform);
+    expect(inferIcon).toHaveBeenCalledWith(
+      ICON_PARAMS_NEEDS_INFER.targetUrl,
+      ICON_PARAMS_NEEDS_INFER.platform,
+    );
   });
 
   describe('when inferIcon resolves with an error', () => {
     test('it should handle the error', async () => {
-      inferIcon.mockImplementationOnce(() =>
+      (inferIcon as jest.Mock).mockImplementationOnce(() =>
         Promise.reject(new Error('some error')),
       );
-      const params = { targetUrl: 'some url', platform: 'mac' };
+      const result = await icon(ICON_PARAMS_NEEDS_INFER);
 
-      const result = await icon(params);
       expect(result).toBe(null);
-      expect(inferIcon).toHaveBeenCalledWith(params.targetUrl, params.platform);
+      expect(inferIcon).toHaveBeenCalledWith(
+        ICON_PARAMS_NEEDS_INFER.targetUrl,
+        ICON_PARAMS_NEEDS_INFER.platform,
+      );
       expect(log.warn).toHaveBeenCalledTimes(1);
     });
   });
--- src/options/fields/icon.ts
+++ src/options/fields/icon.ts
@@ -2,13 +2,24 @@ import { inferIcon } from '../../infer';
 
 import log = require('loglevel');
 
-export async function icon({ icon, targetUrl, platform }): Promise<string> {
-  if (icon) {
-    return icon;
+type IconParamsProvided = {
+  icon: string;
+};
+
+type IconParamsNeedsInfer = {
+  targetUrl: string;
+  platform: string;
+};
+
+type IconParams = IconParamsProvided | IconParamsNeedsInfer;
+
+export async function icon(params: IconParams): Promise<string> {
+  if ('icon' in params) {
+    return params.icon;
   }
 
   try {
-    return await inferIcon(targetUrl, platform);
+    return await inferIcon(params.targetUrl, params.platform);
   } catch (error) {
     log.warn('Cannot automatically retrieve the app icon:', error);
     return null;
