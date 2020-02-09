bb654e454a568d0ee28abdf2b213c88405929139
Ronan Jouchet <ronan@jouchet.fr>
2020-02-09 13:13:42 -0500

    Restore prettier, accidentally removed while adding typed-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,14 +6,16 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'eslint:recommended',
+    'prettier',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
   rules: {
+    'prettier/prettier': 'error',
     // TODO remove when done killing anys and making tsc strict
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/ban-ts-ignore': 'off',
