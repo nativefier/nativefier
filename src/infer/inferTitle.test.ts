d6e7aa6f4188b9696d4e22931961ee18c46d13bc
Ronan Jouchet <ronan@jouchet.fr>
2020-03-15 17:19:19 -0400

    Fix inferTitle test broken on Node 8

--- src/infer/inferTitle.test.ts
+++ src/infer/inferTitle.test.ts
@@ -1,17 +1,22 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 import { inferTitle } from './inferTitle';
 
 test('it returns the correct title', async () => {
   const axiosGetMock = jest.spyOn(axios, 'get');
-  axiosGetMock.mockResolvedValue({
+  const mockedResponse: AxiosResponse<string> = {
     data: `
       <HTML>
         <head>
           <title>TEST_TITLE</title>
         </head>
       </HTML>`,
-  });
+    status: 200,
+    statusText: 'OK',
+    headers: {},
+    config: {},
+  };
+  axiosGetMock.mockResolvedValue(mockedResponse);
   const result = await inferTitle('someurl');
 
   expect(axiosGetMock).toHaveBeenCalledTimes(1);
