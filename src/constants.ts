85629434e69dc141f6a57bd1c47da90e6bc01ab4
Ronan Jouchet <ronan@jouchet.fr>
2020-08-10 21:42:57 -0400

    Bump default Electron to 9.2.0

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 export const DEFAULT_APP_NAME = 'APP';
 
 // Update both together
-export const DEFAULT_ELECTRON_VERSION = '9.1.0';
+export const DEFAULT_ELECTRON_VERSION = '9.2.0';
 export const DEFAULT_CHROME_VERSION = '83.0.4103.122';
 
 export const ELECTRON_MAJOR_VERSION = parseInt(
