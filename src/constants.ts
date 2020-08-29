b1d0d6857f2ced72ea94adadfa56f9b23aab3ca5
Ronan Jouchet <ronan@jouchet.fr>
2020-08-29 16:08:37 -0400

    Bump default Electron to 10.1.0, bump to TS 4.x

--- src/constants.ts
+++ src/constants.ts
@@ -3,8 +3,8 @@ import * as path from 'path';
 export const DEFAULT_APP_NAME = 'APP';
 
 // Update both together
-export const DEFAULT_ELECTRON_VERSION = '9.2.0';
-export const DEFAULT_CHROME_VERSION = '83.0.4103.122';
+export const DEFAULT_ELECTRON_VERSION = '10.1.0';
+export const DEFAULT_CHROME_VERSION = '85.0.4183.87';
 
 export const ELECTRON_MAJOR_VERSION = parseInt(
   DEFAULT_ELECTRON_VERSION.split('.')[0],
