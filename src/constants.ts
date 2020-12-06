7c5b2bb68f66a4a6adc7d8d6dd5ba121309573b8
Ronan Jouchet <ronan@jouchet.fr>
2020-12-06 13:57:27 -0500

    Bump default Electron to 11.0.3, bump dep eslint-config-prettier to 7.x, bump version to something far away from current Electron version
    Rationale for nonsensical major version bump: around Nativefier 8.x,
    versions of Nativefier and Electron aligned, by release schedule coincidence.
    Since Nativefier has little breaking changes, it was great: as Electron
    releases are breaking, Nativefier had no breaking changes, I bumped our
    major version on new major Electron, and everything was good.
    
    Except *now*, as I have a breaking change, that would bump Nativefier to
    12.x, which would be confusing since we'd still default to Electron 11 :-/ .
    
    -> To keep respecting semver and reduce confusion, bumping Nativefier
       version to something far ahead. No it doesn't matter, version
       number are meaningless anyway (well, outside of semver, whose
       respect is precisely the point here).

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 export const DEFAULT_APP_NAME = 'APP';
 
 // Update both together
-export const DEFAULT_ELECTRON_VERSION = '11.0.2';
+export const DEFAULT_ELECTRON_VERSION = '11.0.3';
 export const DEFAULT_CHROME_VERSION = '87.0.4280.67';
 
 export const ELECTRON_MAJOR_VERSION = parseInt(
