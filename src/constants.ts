dcadfb0bcacf6a86809eb3bb79eb8990562a3368
Ronan Jouchet <ronan@jouchet.fr>
2021-03-10 20:25:33 -0500

    Bump to Electron 12.0.1 with Chrome 89.0.4389.82

--- src/constants.ts
+++ src/constants.ts
@@ -3,8 +3,8 @@ import * as path from 'path';
 export const DEFAULT_APP_NAME = 'APP';
 
 // Update both together, and update app / package.json / devDeps / electron
-export const DEFAULT_ELECTRON_VERSION = '11.3.0';
-export const DEFAULT_CHROME_VERSION = '87.0.4280.141';
+export const DEFAULT_ELECTRON_VERSION = '12.0.1';
+export const DEFAULT_CHROME_VERSION = '89.0.4389.82';
 
 export const ELECTRON_MAJOR_VERSION = parseInt(
   DEFAULT_ELECTRON_VERSION.split('.')[0],
