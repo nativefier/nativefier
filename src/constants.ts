36a9e2d6167c5c3841ad6756e53168536be9a45c
Ronan Jouchet <ronan@jouchet.fr>
2021-05-01 10:48:29 -0400

    Bump to Electron 12.0.6 with security fixes
    See https://github.com/electron/electron/releases/tag/v12.0.6

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 export const DEFAULT_APP_NAME = 'APP';
 
 // Update both together, and update app / package.json / devDeps / electron
-export const DEFAULT_ELECTRON_VERSION = '12.0.5';
+export const DEFAULT_ELECTRON_VERSION = '12.0.6';
 export const DEFAULT_CHROME_VERSION = '89.0.4389.128';
 
 export const ELECTRON_MAJOR_VERSION = parseInt(
