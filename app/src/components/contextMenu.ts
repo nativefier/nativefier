461cbb4707766c53df445cf46f9c62e7383c8e31
Adam Weeden <adamweeden@gmail.com>
2021-05-25 18:39:06 -0400

    Fix contextMenu require bug

--- app/src/components/contextMenu.ts
+++ app/src/components/contextMenu.ts
@@ -6,10 +6,10 @@ export function initContextMenu(
   openExternal,
   window?: BrowserWindow,
 ): void {
-  // Require this at call time, otherwise its child dependency 'electron-is-dev'
+  // Require this at runtime, otherwise its child dependency 'electron-is-dev'
   // throws an error during unit testing.
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const { contextMenu } = require('electron-context-menu');
+  const contextMenu = require('electron-context-menu');
 
   contextMenu({
     prepend: (actions, params) => {
