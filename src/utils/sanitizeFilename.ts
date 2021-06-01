883b27e54d48fad95057f27fec9222e0fa7710f5
Adam Weeden <adamweeden@gmail.com>
2021-05-31 23:22:13 -0400

    Fix prettier

--- src/utils/sanitizeFilename.ts
+++ src/utils/sanitizeFilename.ts
@@ -16,7 +16,9 @@ export function sanitizeFilename(
 
   if (!result || result === '') {
     result = DEFAULT_APP_NAME;
-    log.warn('Falling back to default app name as result of filename sanitization. Use flag "--name" to set a name');
+    log.warn(
+      'Falling back to default app name as result of filename sanitization. Use flag "--name" to set a name',
+    );
   }
   log.debug(`Sanitized filename for ${filenameToSanitize} : ${result}`);
   return result;
