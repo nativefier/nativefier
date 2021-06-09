9a6c6f870df75ca2a580128d8c429cf5071d6bad
Adam Weeden <adamweeden@gmail.com>
2021-06-09 09:49:35 -0400

    Selective css injection (#1222)
    * Don't inject CSS for unneeded responses
    
    * Remove some non-injectable methods
    
    * Actually check for CSS to inject + unit tests
    
    * Update app/src/helpers/windowHelpers.ts
    
    Co-authored-by: Ronan Jouchet <ronan@jouchet.fr>
    
    Co-authored-by: Ronan Jouchet <ronan@jouchet.fr>

--- app/src/helpers/windowHelpers.ts
+++ app/src/helpers/windowHelpers.ts
@@ -10,12 +10,7 @@ import {
 
 import log from 'loglevel';
 import path from 'path';
-import {
-  getCSSToInject,
-  isOSX,
-  nativeTabsSupported,
-  shouldInjectCSS,
-} from './helpers';
+import { getCSSToInject, isOSX, nativeTabsSupported } from './helpers';
 
 const ZOOM_INTERVAL = 0.1;
 
@@ -198,11 +193,11 @@ export function hideWindow(
 }
 
 export function injectCSS(browserWindow: BrowserWindow): void {
-  if (!shouldInjectCSS()) {
+  const cssToInject = getCSSToInject();
+
+  if (!cssToInject) {
     return;
   }
-
-  const cssToInject = getCSSToInject();
 
   browserWindow.webContents.on('did-navigate', () => {
     log.debug(
@@ -218,31 +213,52 @@ export function injectCSS(browserWindow: BrowserWindow): void {
         details: OnHeadersReceivedListenerDetails,
         callback: (headersReceivedResponse: HeadersReceivedResponse) => void,
       ) => {
-        log.debug(
-          'browserWindow.webContents.session.webRequest.onHeadersReceived',
-          { details, callback },
-        );
-        if (details.webContents) {
-          details.webContents
-            .insertCSS(cssToInject)
-            .catch((err) => {
-              log.error('webContents.insertCSS ERROR', err);
-            })
-            .finally(() =>
-              callback({
-                cancel: false,
-                responseHeaders: details.responseHeaders,
-              }),
-            );
-        } else {
-          callback({
-            cancel: false,
-            responseHeaders: details.responseHeaders,
+        injectCSSIntoResponse(details, cssToInject)
+          .then((responseHeaders) => {
+            callback({
+              cancel: false,
+              responseHeaders,
+            });
+          })
+          .catch((err) => {
+            log.error('injectCSSIntoResponse ERROR', err);
+            callback({
+              cancel: false,
+              responseHeaders: details.responseHeaders,
+            });
           });
-        }
       },
     );
   });
+}
+
+async function injectCSSIntoResponse(
+  details: OnHeadersReceivedListenerDetails,
+  cssToInject: string,
+): Promise<Record<string, string[]>> {
+  // We go with a denylist rather than a whitelist (e.g. only GET/html)
+  // to avoid "whoops I didn't think this should have been CSS-injected" cases
+  const nonInjectableMethods = ['DELETE', 'OPTIONS'];
+  const nonInjectableResourceTypes = ['image', 'script', 'stylesheet', 'xhr'];
+
+  if (
+    nonInjectableMethods.includes(details.method) ||
+    nonInjectableResourceTypes.includes(details.resourceType) ||
+    !details.webContents
+  ) {
+    log.debug(
+      `Skipping CSS injection for:\n${details.url}\nwith method ${details.method} and resourceType ${details.resourceType} and content-type ${details.responseHeaders['content-type']}`,
+    );
+    return details.responseHeaders;
+  }
+
+  log.debug('browserWindow.webContents.session.webRequest.onHeadersReceived', {
+    details,
+    contentType: details.responseHeaders['content-type'],
+  });
+  await details.webContents.insertCSS(cssToInject);
+
+  return details.responseHeaders;
 }
 
 export function sendParamsOnDidFinishLoad(
