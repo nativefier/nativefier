97b33b369f0627b9a6bf6e1614471f8bc1e2aa0d
Ronan Jouchet <ronan@jouchet.fr>
2021-07-10 15:59:38 -0400

    Bump default Electron from 12.0.12 to 12.0.14
    - https://github.com/electron/electron/releases/tag/v12.0.13
    - https://github.com/electron/electron/releases/tag/v12.0.14

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ export const DEFAULT_APP_NAME = 'APP';
 
 // Update both DEFAULT_ELECTRON_VERSION and DEFAULT_CHROME_VERSION together,
 // and update app / package.json / devDeps / electron to value of DEFAULT_ELECTRON_VERSION
-export const DEFAULT_ELECTRON_VERSION = '12.0.12';
+export const DEFAULT_ELECTRON_VERSION = '12.0.14';
 export const DEFAULT_CHROME_VERSION = '89.0.4389.128';
 
 // Update each of these periodically
