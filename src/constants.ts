eec69bf3bb2aebdc7dfa2ecbaf602a752c140a1f
Adam Weeden <adamweeden@gmail.com>
2021-07-15 09:56:56 -0400

    13.1.6 -> NO CRASH!

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,8 @@ export const DEFAULT_APP_NAME = 'APP';
 
 // Update both DEFAULT_ELECTRON_VERSION and DEFAULT_CHROME_VERSION together,
 // and update app / package.json / devDeps / electron to value of DEFAULT_ELECTRON_VERSION
-export const DEFAULT_ELECTRON_VERSION = '13.1.4';
-export const DEFAULT_CHROME_VERSION = '91.0.4472.106';
+export const DEFAULT_ELECTRON_VERSION = '13.1.6';
+export const DEFAULT_CHROME_VERSION = '91.0.4472.124';
 
 // Update each of these periodically
 // https://product-details.mozilla.org/1.0/firefox_versions.json
