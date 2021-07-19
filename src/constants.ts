bf4be860cfc5be12384df24649e34e094054dc60
Adam Weeden <adamweeden@gmail.com>
2021-07-19 13:03:46 -0400

    Upgrade to Electron 13 (#1230)
    * Catch promise errors better
    
    * Move subFunctions to bottom of createNewWindow
    
    * Use parents when creating child BrowserWindow instances
    
    * Some about:blank pages have an anchor (for some reason)
    
    * Inject browserWindowOptions better
    
    * Interim refactor to MainWindow object
    
    * Split up the window functions/helpers/events some
    
    * Further separate out window functions + tests
    
    * Add a mock for unit testing functions that access electron
    
    * Add unit tests for onWillPreventUnload
    
    * Improve windowEvents tests
    
    * Add the first test for windowHelpers
    
    * Move WebRequest event handling to node
    
    * insertCSS completely under test
    
    * clearAppData completely under test
    
    * Fix contextMenu require bug
    
    * More tests + fixes
    
    * Fix + add to createNewTab tests
    
    * Convert createMainWindow back to func + work out gremlins
    
    * Move setupWindow away from main since its shared
    
    * Make sure contextMenu is handling promises
    
    * v13.1.2
    
    * v13.1.4
    
    * Update Webkit version for Safari
    
    * 13.1.6 -> NO CRASH!
    
    * Fix types/debug build error on Ubuntu
    
    * 13 -> 13.1.7
    
    * Bump default Firefox version
    
    Co-authored-by: Ronan Jouchet <ronan@jouchet.fr>

--- src/constants.ts
+++ src/constants.ts
@@ -4,18 +4,18 @@ export const DEFAULT_APP_NAME = 'APP';
 
 // Update both DEFAULT_ELECTRON_VERSION and DEFAULT_CHROME_VERSION together,
 // and update app / package.json / devDeps / electron to value of DEFAULT_ELECTRON_VERSION
-export const DEFAULT_ELECTRON_VERSION = '12.0.14';
-export const DEFAULT_CHROME_VERSION = '89.0.4389.128';
+export const DEFAULT_ELECTRON_VERSION = '13.1.7';
+export const DEFAULT_CHROME_VERSION = '91.0.4472.124';
 
 // Update each of these periodically
 // https://product-details.mozilla.org/1.0/firefox_versions.json
-export const DEFAULT_FIREFOX_VERSION = '89.0';
+export const DEFAULT_FIREFOX_VERSION = '90.0';
 
 // https://en.wikipedia.org/wiki/Safari_version_history
 export const DEFAULT_SAFARI_VERSION = {
   majorVersion: 14,
-  version: '14.0.3',
-  webkitVersion: '610.4.3.1.7',
+  version: '14.1.1',
+  webkitVersion: '611.2.7.1.4',
 };
 
 export const ELECTRON_MAJOR_VERSION = parseInt(
