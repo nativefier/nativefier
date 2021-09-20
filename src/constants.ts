e222e99b56f7f3b8a1f8c5b42dc497d1da77a765
Ronan Jouchet <ronan@jouchet.fr>
2021-09-20 11:38:15 -0400

    Bump default Electron to 13.4.0

--- src/constants.ts
+++ src/constants.ts
@@ -4,18 +4,18 @@ export const DEFAULT_APP_NAME = 'APP';
 
 // Update both DEFAULT_ELECTRON_VERSION and DEFAULT_CHROME_VERSION together,
 // and update app / package.json / devDeps / electron to value of DEFAULT_ELECTRON_VERSION
-export const DEFAULT_ELECTRON_VERSION = '13.1.7';
+export const DEFAULT_ELECTRON_VERSION = '13.4.0';
 export const DEFAULT_CHROME_VERSION = '91.0.4472.124';
 
 // Update each of these periodically
 // https://product-details.mozilla.org/1.0/firefox_versions.json
-export const DEFAULT_FIREFOX_VERSION = '90.0';
+export const DEFAULT_FIREFOX_VERSION = '92.0';
 
 // https://en.wikipedia.org/wiki/Safari_version_history
 export const DEFAULT_SAFARI_VERSION = {
   majorVersion: 14,
-  version: '14.1.1',
-  webkitVersion: '611.2.7.1.4',
+  version: '14.1.2',
+  webkitVersion: '611.3.10.1.5',
 };
 
 export const ELECTRON_MAJOR_VERSION = parseInt(
