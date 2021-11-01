94e6c6cf54e2afb36dd01a752aff2106e8ce8114
Ronan Jouchet <ronan@jouchet.fr>
2021-11-01 15:34:19 -0400

    Bump default Electron to 13.6.1, with security fixes
    - 13.5.2: https://github.com/electron/electron/releases/tag/v13.5.2
    - 13.6.0: https://github.com/electron/electron/releases/tag/v13.6.0
    - 13.6.1: https://github.com/electron/electron/releases/tag/v13.6.1

--- src/constants.ts
+++ src/constants.ts
@@ -4,13 +4,13 @@ export const DEFAULT_APP_NAME = 'APP';
 
 // Update both DEFAULT_ELECTRON_VERSION and DEFAULT_CHROME_VERSION together,
 // and update app / package.json / devDeps / electron to value of DEFAULT_ELECTRON_VERSION
-export const DEFAULT_ELECTRON_VERSION = '13.5.1';
+export const DEFAULT_ELECTRON_VERSION = '13.6.1';
 // https://atom.io/download/atom-shell/index.json
 export const DEFAULT_CHROME_VERSION = '91.0.4472.164';
 
 // Update each of these periodically
 // https://product-details.mozilla.org/1.0/firefox_versions.json
-export const DEFAULT_FIREFOX_VERSION = '92.0.1';
+export const DEFAULT_FIREFOX_VERSION = '93.0';
 
 // https://en.wikipedia.org/wiki/Safari_version_history
 export const DEFAULT_SAFARI_VERSION = {
