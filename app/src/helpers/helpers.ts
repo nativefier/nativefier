71e765c76dd73c1126d55ba3e52e25ec1c9bd9c4
Rick Stanley <rick-stanley@outlook.com>
2021-12-06 19:58:05 -0400

    fix(linter): eslint errors

--- app/src/helpers/helpers.ts
+++ app/src/helpers/helpers.ts
@@ -263,7 +263,11 @@ export function openExternal(
 }
 
 export function isWayland(): boolean {
-  return isLinux() && (Boolean(process.env.WAYLAND_DISPLAY) || process.env.XDG_SESSION_TYPE === 'wayland'); 
+  return (
+    isLinux() &&
+    (Boolean(process.env.WAYLAND_DISPLAY) ||
+      process.env.XDG_SESSION_TYPE === 'wayland')
+  );
 }
 
 export function removeUserAgentSpecifics(
