5c5d7afc8e3e6ff32da4bde60e82052b2627ff1b
Rick Stanley <rick.stanley@lambda3.com.br>
2021-12-07 00:43:51 -0400

    fix(lint): fix linter errors

--- app/src/components/mainWindow.ts
+++ app/src/components/mainWindow.ts
@@ -232,7 +232,7 @@ function setupSessionPermissionHandler(window: BrowserWindow): void {
     },
   );
   window.webContents.session.setPermissionRequestHandler(
-    async (_webContents, _permission, callback, _details) => {
+    (_webContents, _permission, callback, _details) => {
       callback(true);
     },
   );
