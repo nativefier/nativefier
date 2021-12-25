b6cd52c18eac314f677d6909a6762be89cd8598c
Ronan Jouchet <ronan@jouchet.fr>
2021-12-25 09:37:50 -0500

    first lets do a minor bump to latest electron 14; 14.2.3

--- src/constants.ts
+++ src/constants.ts
@@ -4,13 +4,13 @@ export const DEFAULT_APP_NAME = 'APP';
 
 // Update both DEFAULT_ELECTRON_VERSION and DEFAULT_CHROME_VERSION together,
 // and update app / package.json / devDeps / electron to value of DEFAULT_ELECTRON_VERSION
-export const DEFAULT_ELECTRON_VERSION = '14.1.0';
+export const DEFAULT_ELECTRON_VERSION = '14.2.3';
 // https://atom.io/download/atom-shell/index.json
 export const DEFAULT_CHROME_VERSION = '93.0.4577.82';
 
 // Update each of these periodically
 // https://product-details.mozilla.org/1.0/firefox_versions.json
-export const DEFAULT_FIREFOX_VERSION = '94.0.2';
+export const DEFAULT_FIREFOX_VERSION = '95.0.2';
 
 // https://en.wikipedia.org/wiki/Safari_version_history
 export const DEFAULT_SAFARI_VERSION = {
