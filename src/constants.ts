ad74271b985ae38a0b1109ca0486dd1a6a1af5ea
Ronan Jouchet <ronan@jouchet.fr>
2022-02-14 23:03:47 -0500

    Bump default Electron to 16.0.9, from 16.0.8

--- src/constants.ts
+++ src/constants.ts
@@ -4,13 +4,13 @@ export const DEFAULT_APP_NAME = 'APP';
 
 // Update both DEFAULT_ELECTRON_VERSION and DEFAULT_CHROME_VERSION together,
 // and update app / package.json / devDeps / electron to value of DEFAULT_ELECTRON_VERSION
-export const DEFAULT_ELECTRON_VERSION = '16.0.8';
+export const DEFAULT_ELECTRON_VERSION = '16.0.9';
 // https://atom.io/download/atom-shell/index.json
-export const DEFAULT_CHROME_VERSION = '96.0.4664.110';
+export const DEFAULT_CHROME_VERSION = '96.0.4664.174';
 
 // Update each of these periodically
 // https://product-details.mozilla.org/1.0/firefox_versions.json
-export const DEFAULT_FIREFOX_VERSION = '96.0.3';
+export const DEFAULT_FIREFOX_VERSION = '97.0';
 
 // https://en.wikipedia.org/wiki/Safari_version_history
 export const DEFAULT_SAFARI_VERSION = {
