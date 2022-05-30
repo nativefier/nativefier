c82f1a892e2dee22b5d16d5ca250962b4b5d67f1
Ronan Jouchet <ronan@jouchet.fr>
2022-05-30 09:58:42 -0400

    Bump default Electron to 18.3.1
    https://github.com/electron/electron/releases/tag/v18.2.1
    https://github.com/electron/electron/releases/tag/v18.2.2
    https://github.com/electron/electron/releases/tag/v18.2.3
    https://github.com/electron/electron/releases/tag/v18.2.4
    https://github.com/electron/electron/releases/tag/v18.3.0
    https://github.com/electron/electron/releases/tag/v18.3.1

--- src/constants.ts
+++ src/constants.ts
@@ -5,13 +5,13 @@ export const DEFAULT_APP_NAME = 'APP';
 // Update both DEFAULT_ELECTRON_VERSION and DEFAULT_CHROME_VERSION together,
 // and update package.json / devDeps / electron to value of DEFAULT_ELECTRON_VERSION
 // and update app / package.json / devDeps / electron to value of DEFAULT_ELECTRON_VERSION
-export const DEFAULT_ELECTRON_VERSION = '18.2.0';
+export const DEFAULT_ELECTRON_VERSION = '18.3.1';
 // https://atom.io/download/atom-shell/index.json
-export const DEFAULT_CHROME_VERSION = '100.0.4896.143';
+export const DEFAULT_CHROME_VERSION = '100.0.4896.160';
 
 // Update each of these periodically
 // https://product-details.mozilla.org/1.0/firefox_versions.json
-export const DEFAULT_FIREFOX_VERSION = '99.0.1';
+export const DEFAULT_FIREFOX_VERSION = '100.0.2';
 
 // https://en.wikipedia.org/wiki/Safari_version_history
 export const DEFAULT_SAFARI_VERSION = {
