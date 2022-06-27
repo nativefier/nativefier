bac08394c5b17bc0b2ac0ba307325be73c3a8c8e
Ronan Jouchet <ronan@jouchet.fr>
2022-06-27 09:34:13 -0400

    macOS: fix incorrect "Back" keyboard shortcut (fix #1426)

--- app/src/components/menu.ts
+++ app/src/components/menu.ts
@@ -153,7 +153,7 @@ export function generateMenu(
     submenu: [
       {
         label: 'Back',
-        accelerator: isOSX() ? 'CmdOrAlt+Left' : 'Alt+Left',
+        accelerator: isOSX() ? 'Cmd+Left' : 'Alt+Left',
         click: goBack,
       },
       {
