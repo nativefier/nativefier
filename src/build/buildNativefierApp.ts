c8fc0b692364125d588bdca0ef885815362f2e65
Adam Weeden <adamweeden@gmail.com>
2022-08-01 22:44:23 -0400

    macOS: Move handling of "universal" apps to electron-packager instead of our own thing (#1443)
    Supported since [electron-packager 15.5.0](https://github.com/electron/electron-packager/releases/tag/v15.5.0).
    
    This should fix #1405  as well

--- src/build/buildNativefierApp.ts
+++ src/build/buildNativefierApp.ts
@@ -13,14 +13,9 @@ import {
   isWindowsAdmin,
 } from '../helpers/helpers';
 import { useOldAppOptions, findUpgradeApp } from '../helpers/upgrade/upgrade';
-import {
-  AppOptions,
-  OutputOptions,
-  RawOptions,
-} from '../../shared/src/options/model';
-import { getOptions, normalizePlatform } from '../options/optionsMain';
+import { AppOptions, RawOptions } from '../../shared/src/options/model';
+import { getOptions } from '../options/optionsMain';
 import { prepareElectronApp } from './prepareElectronApp';
-import { makeUniversalApp } from '@electron/universal';
 
 const OPTIONS_REQUIRING_WINDOWS_FOR_WINDOWS_BUILD = [
   'icon',
@@ -130,19 +125,6 @@ function trimUnprocessableOptions(options: AppOptions): void {
   }
 }
 
-function isInvalidUniversal(options: RawOptions): boolean {
-  const platform = normalizePlatform(options.platform);
-  if (
-    (options.arch ?? '').toLowerCase() === 'universal' &&
-    platform !== 'darwin' &&
-    platform !== 'mas'
-  ) {
-    return true;
-  }
-
-  return false;
-}
-
 function getOSRunHelp(platform?: string): string {
   if (platform === 'win32') {
     return `the contained .exe file.`;
@@ -219,6 +201,8 @@ export async function buildNativefierApp(
   convertIconIfNecessary(options);
   await copyIconsIfNecessary(options, tmpPath);
 
+  options.packager.quiet = !rawOptions.verbose;
+
   log.info(
     "\nPackaging... This will take a few seconds, maybe minutes if the requested Electron isn't cached yet...",
   );
@@ -280,95 +264,4 @@ export async function buildNativefierApp(
   );
 
   return appPath;
-}
-
-function modifyOptionsForUniversal(appPath: string, buildDate: number): void {
-  const nativefierJSONPath = path.join(
-    appPath,
-    'Contents',
-    'Resources',
-    'app',
-    'nativefier.json',
-  );
-  const options = JSON.parse(
-    fs.readFileSync(nativefierJSONPath, 'utf8'),
-  ) as OutputOptions;
-  options.arch = 'universal';
-  options.buildDate = buildDate;
-  fs.writeFileSync(nativefierJSONPath, JSON.stringify(options, null, 2));
-}
-
-export async function buildUniversalApp(options: RawOptions): Promise<string> {
-  if (isInvalidUniversal(options)) {
-    throw new Error(
-      'arch of "universal" can only be used with Mac OS app types.',
-    );
-  }
-
-  const platform = normalizePlatform(options.platform);
-
-  const x64Options = { ...options, arch: 'x64' };
-  const arm64Options = { ...options, arch: 'arm64' };
-
-  log.info('Creating universal Mac binary...');
-
-  let x64Path: string | undefined;
-  let arm64Path: string | undefined;
-  try {
-    x64Path = path.resolve(await buildNativefierApp(x64Options));
-    arm64Path = path.resolve(await buildNativefierApp(arm64Options));
-    const universalAppPath = path
-      .join(
-        x64Path,
-        `${path.parse(x64Path).base.replace(`-${platform}-x64`, '')}.app`,
-      )
-      .replace('x64', 'universal');
-    const x64AppPath = path.join(
-      x64Path,
-      `${path.parse(x64Path).base.replace(`-${platform}-x64`, '')}.app`,
-    );
-    const arm64AppPath = path.join(
-      arm64Path,
-      `${path.parse(arm64Path).base.replace(`-${platform}-arm64`, '')}.app`,
-    );
-    // We're going to change the nativefier.json on these to match otherwise we'll see:
-    // Expected all non-binary files to have identical SHAs when creating a universal build but "Google.app/Contents/Resources/app/nativefier.json" did not
-    const buildDate = new Date().getTime();
-    modifyOptionsForUniversal(x64AppPath, buildDate);
-    modifyOptionsForUniversal(arm64AppPath, buildDate);
-    await makeUniversalApp({
-      x64AppPath,
-      arm64AppPath,
-      outAppPath: universalAppPath,
-      force: !!options.overwrite,
-    });
-
-    await fs.copyFile(
-      path.join(x64Path, 'LICENSE'),
-      path.join(universalAppPath, '..', 'LICENSE'),
-    );
-
-    await fs.copyFile(
-      path.join(x64Path, 'LICENSES.chromium.html'),
-      path.join(universalAppPath, '..', 'LICENSES.chromium.html'),
-    );
-
-    await fs.copyFile(
-      path.join(x64Path, 'version'),
-      path.join(universalAppPath, '..', 'version'),
-    );
-
-    const osRunHelp = getOSRunHelp(platform);
-    log.info(
-      `App built to ${universalAppPath}, move to wherever it makes sense for you and run ${osRunHelp}`,
-    );
-    return universalAppPath;
-  } finally {
-    if (x64Path) {
-      fs.removeSync(x64Path);
-    }
-    if (arm64Path) {
-      fs.removeSync(arm64Path);
-    }
-  }
 }
