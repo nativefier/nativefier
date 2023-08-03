051622d58e9a9b0651990ed767063ee74c3f076a
Adam Weeden <adamweeden@gmail.com>
2023-08-03 13:35:19 -0400

    Update Electron to 21 + Node to 16 (#1550)

--- README.md
+++ README.md
@@ -31,8 +31,8 @@ Whatsapp Web ([HN thread](https://news.ycombinator.com/item?id=10930718)). Nativ
 
 Install Nativefier globally with `npm install -g nativefier` . Requirements:
 
-- macOS 10.10+ / Windows / Linux
-- [Node.js](https://nodejs.org/) ≥ 12.9 and npm ≥ 6.9
+- macOS 10.13+ / Windows / Linux
+- [Node.js](https://nodejs.org/) ≥ 16.9 and npm ≥ 7.10
 
 Optional dependencies:
 
--- src/constants.ts
+++ src/constants.ts
@@ -6,20 +6,20 @@ export const DEFAULT_APP_NAME = 'APP';
 //   - upgrade app / package.json / "devDependencies" / "electron"
 //   - upgrade       package.json / "devDependencies" / "electron"
 // Doing a *major* upgrade? Read https://github.com/nativefier/nativefier/blob/master/HACKING.md#deps-major-upgrading-electron
-export const DEFAULT_ELECTRON_VERSION = '19.1.4';
+export const DEFAULT_ELECTRON_VERSION = '21.4.4';
 // https://atom.io/download/atom-shell/index.json
 // https://www.electronjs.org/releases/stable
-export const DEFAULT_CHROME_VERSION = '102.0.5005.167';
+export const DEFAULT_CHROME_VERSION = '106.0.5249.199';
 
 // Update each of these periodically
 // https://product-details.mozilla.org/1.0/firefox_versions.json
-export const DEFAULT_FIREFOX_VERSION = '106.0.5';
+export const DEFAULT_FIREFOX_VERSION = '116.0';
 
 // https://en.wikipedia.org/wiki/Safari_version_history
 export const DEFAULT_SAFARI_VERSION = {
-  majorVersion: 15,
-  version: '15.6.1',
-  webkitVersion: '613.2.7',
+  majorVersion: 65,
+  version: '16.5.2',
+  webkitVersion: '605.1.15',
 };
 
 export const ELECTRON_MAJOR_VERSION = parseInt(
